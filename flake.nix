4b344dcc5d7cff86977ebfd2bdd34dc33adbc41d
Laureηt <laurent@fainsin.bzh>
2023-08-08 21:22:42 +0200

    ⬆️ nix flake update

--- flake.nix
+++ flake.nix
@@ -7,8 +7,5 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        devShell =
-          pkgs.mkShell { buildInputs = with pkgs; [ julia python3 nodejs ]; };
-      });
+      in { devShell = pkgs.mkShell { buildInputs = with pkgs; [ julia ]; }; });
 }
