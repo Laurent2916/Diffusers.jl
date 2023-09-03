9689c9f2d0052b961c911806f707eb6ef56bddf7
Laureηt <laurent@fainsin.bzh>
2023-09-03 14:45:40 +0200

    📝 add DocumenterCitations and a couple references

--- docs/make.jl
+++ docs/make.jl
@@ -1,16 +1,18 @@
 using Diffusers
 using Documenter
+using DocumenterCitations
 
 DocMeta.setdocmeta!(Diffusers, :DocTestSetup, :(using Diffusers); recursive=true)
+bib = CitationBibliography(joinpath(@__DIR__, "src", "refs.bib"))
 
-makedocs(;
+makedocs(bib;
   modules=[Diffusers],
   authors="Laurent Fainsin <laurent@fainsin.bzh>",
   repo="https://github.com/Laurent2916/Diffusers.jl/blob/{commit}{path}#{line}",
   sitename="Diffusers.jl",
   format=Documenter.HTML(;
     prettyurls=true,
-    edit_link="main",
+    edit_link="master",
     assets=String[]
   ),
   linkcheck=true,
@@ -18,6 +20,7 @@ makedocs(;
     "Home" => "index.md",
     "Schedulers" => "schedulers.md",
     "Beta Schedules" => "beta_schedules.md",
+    "References" => "references.md",
   ]
 )
 
